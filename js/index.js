7e377c071f4f3a01b646d5e0aa01703208df8a60
mahfuzuddin <mahfuzuddin347@gmai.com>
2018-10-29 17:46:06 -0400

    firebase is connected and ready for use

--- js/index.js
+++ js/index.js
@@ -4,3 +4,12 @@ import test from './script2.js';
 console.log('test');
 
 test();
+
+var database = firebase.database();
+var databaseRef = database.ref('/');
+databaseRef.once('value').then(function(snapshot){
+
+    const databaseValues = snapshot.val();
+
+    console.log(databaseValues);
+});
